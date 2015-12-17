8901b0b598baea97fb53a702d4e60502248e0fa7
ics-ikeda <ikeda@ics-web.jp>
2015-12-17 22:59:04 +0900

    テンプレート : フッター周りの調整

--- html/js/main.ts
+++ html/js/main.ts
@@ -11,5 +11,15 @@ namespace project {
     $("article a").removeAttr("target");
     $("article a[href^=http]").attr("target", "_blank");
     //$("a[href^=http://]").not($("a[href^=https://ics.media]")).attr("target", "_blank");
+
+    console.log(location.href);
+    console.log(location.href.indexOf("index.html"));
+    if (location.href.indexOf("index.html") > -1) {
+      $("div.link-index").hide();
+    }
+
+    $("a.share-twitter").attr("href", `https://twitter.com/share?text=${encodeURI($.trim($("h1").text()) + " - ICS MEDIA " + location.href)}&lang=ja&url=${location.href}`);
+    $("a.share-facebook").attr("href", `https://www.facebook.com/sharer/sharer.php?u=${location.href}`);
+
   }
 }
