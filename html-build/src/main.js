ec56aad98c494dc345f0ade4dfe76732c55074ce
ics-ikeda <ikeda@ics-web.jp>
2018-02-20 18:35:20 +0900

    開発環境をgulpからwebpackへ移行

---
+++ html-build/src/main.js
@@ -0,0 +1,6 @@
+import "./vender/bootstrap/bootstrap.min.css";
+import "./vender/font-awesome/css/font-awesome.css";
+import "./vender/highlight.js/vs.min.css";
+
+import "./style/style.scss";
+import "./scripts/main.js";
