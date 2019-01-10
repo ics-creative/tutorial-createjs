43f4c5e1476e9ff4826172bb4e45babd736b0172
ics-ikeda <ikeda@ics-web.jp>
2019-01-10 13:15:18 +0900

    ビルド環境を更新

--- html-build/webpack.config.js
+++ html-build/webpack.config.js
@@ -1,7 +1,4 @@
-const webpack = require('webpack');
-
 const isProduction = true;
-const useOptimize = isProduction;
 const useSourceMap = !isProduction;
 
 module.exports = {
@@ -22,11 +19,9 @@ module.exports = {
             // Babel のオプションを指定する
             options: {
               presets: [
-                // env を指定することで、ES2017 を ES5 に変換。
-                // {modules: false}にしないと import 文が Babel によって CommonJS に変換され、
-                // webpack の Tree Shaking 機能が使えない
-                ['env', {'modules': false}]
-              ]
+                // プリセットを指定することで、ES2018 を ES5 に変換
+                '@babel/preset-env',
+              ],
             }
           }
         ],
@@ -70,6 +65,5 @@ module.exports = {
         use: 'url-loader'
       }
     ],
-  },
-  plugins: []
+  }
 };
